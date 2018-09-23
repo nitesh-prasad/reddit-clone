fe153d315f747993a8e5f47eb0b72f78d150da4a
nitesh-prasad <prasad.nitesh1996@gmail.com>
2018-09-23 11:17:49 +0530

    Added devise gem and User model

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
@@ -58,4 +60,5 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
